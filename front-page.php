1c95ba8ba4cdacfe8dad28b86da775a68760796a
Matthew Wilber <mwilber@gmail.com>
2025-12-30 21:54:12 -0500

    Group home page cards
    - list articles then projects
    
    - add group spacing

--- front-page.php
+++ front-page.php
@@ -21,8 +21,27 @@ get_header();
     );
     ?>
     <?php if ( $cards_query->have_posts() ) : ?>
-      <section class="card-grid card-grid--front">
+      <section class="card-grid card-grid--front card-grid--group">
         <?php while ( $cards_query->have_posts() ) : $cards_query->the_post(); ?>
+          <?php get_template_part( 'template-parts/card', null, array( 'post_id' => get_the_ID() ) ); ?>
+        <?php endwhile; ?>
+      </section>
+      <?php wp_reset_postdata(); ?>
+    <?php endif; ?>
+
+    <?php
+    $projects_query = new WP_Query(
+      array(
+        'post_type' => 'project',
+        'posts_per_page' => get_option( 'posts_per_page' ),
+        'orderby' => 'date',
+        'order' => 'DESC',
+      )
+    );
+    ?>
+    <?php if ( $projects_query->have_posts() ) : ?>
+      <section class="card-grid card-grid--group">
+        <?php while ( $projects_query->have_posts() ) : $projects_query->the_post(); ?>
           <?php get_template_part( 'template-parts/card', null, array( 'post_id' => get_the_ID() ) ); ?>
         <?php endwhile; ?>
       </section>
